954ab7f97d9713d9a6764609ad4cdc9c6f0e71d8
tim <tim@timdev.de>
2022-08-01 15:16:02 +0200

    fix: fix wrong file path on build

--- index.js
+++ index.js
@@ -17,24 +17,24 @@ var binary = os.platform() === 'win32' ? 'ffmpeg.exe' : 'ffmpeg';
 
 var topLevelPath = path.resolve(__dirname.substr(0, __dirname.indexOf('node_modules')), 'node_modules', '@ffmpeg-installer', platform);
 var npm3Path = path.resolve(__dirname, '..', platform);
-var npm2Path = path.resolve(__dirname, 'node_modules', '@ffmpeg-installer', platform);
+// var npm2Path = path.resolve(__dirname, 'node_modules', '@ffmpeg-installer', platform);
 
 var topLevelBinary = path.join(topLevelPath, binary);
 var npm3Binary = path.join(npm3Path, binary);
-var npm2Binary = path.join(npm2Path, binary);
+// var npm2Binary = path.join(npm2Path, binary);
 
 var topLevelPackage = path.join(topLevelPath, 'package.json');
 var npm3Package = path.join(npm3Path, 'package.json');
-var npm2Package = path.join(npm2Path, 'package.json');
+// var npm2Package = path.join(npm2Path, 'package.json');
 
 var ffmpegPath, packageJson;
 
 if (verifyFile(npm3Binary)) {
     ffmpegPath = npm3Binary;
     packageJson = require(npm3Package);
-} else if (verifyFile(npm2Binary)) {
-    ffmpegPath = npm2Binary;
-    packageJson = require(npm2Package);
+// } else if (verifyFile(npm2Binary)) {
+//     ffmpegPath = npm2Binary;
+//     packageJson = require(npm2Package);
 } else if (verifyFile(topLevelBinary)) {
     ffmpegPath = topLevelBinary;
     packageJson = require(topLevelPackage);
